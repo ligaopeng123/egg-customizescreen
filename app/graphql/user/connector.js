01eb4e5ece7f70e2f599e7e48c1340dcf627ae96
ligaopeng123 <1170234932@qq.com>
2020-09-13 21:04:37 +0800

    合入人：pgli 合入原因：基类开发 合入时间：2020-9-13

--- app/graphql/user/connector.js
+++ app/graphql/user/connector.js
@@ -1,14 +1,18 @@
 'use strict';
 
-const DataLoader = require('dataloader');
-const _ = require('lodash');
-const {Op} = require('sequelize');
-const AppUtils = require('../../AppUtils');
+const TableConnector = require('../../share/table');
 
-class UserConnector {
+class UserConnector extends TableConnector {
     constructor(ctx, app) {
-        this.ctx = ctx;
-        this.loader = new DataLoader(this.fetch.bind(this));
+        super(ctx, app);
+    }
+
+    /**
+     * 初始化模型
+     */
+    init() {
+        this.model = this.ctx.app.model.User;
+        this.name = `用户`;
     }
 
     /**
@@ -42,124 +46,18 @@ class UserConnector {
         })
     }
 
-    /**
-     * DataLoader缓存数据
-     * @param ids
-     * @returns {Promise.<*[]>}
-     */
-    fetch(names) {
-        const users = this.ctx.app.model.User.findAll({
-            where: {
-                name: names,
-            }
-        });
-        return new Promise((resolve, reject) => {
-            users.then(res => {
-                res.length ? resolve(res) : resolve([{}]);
-            })
-        })
-    }
-
-    /**
-     * 查询多个用户信息
-     * @param names
-     * @returns {Promise.<Array.<V|Error>>|Promise<Array<Error | V>>}
-     */
-    fetchByIds(names) {
-        return this.loader.loadMany(names);
-    }
-
-    /**
-     * 查询所有
-     * @returns {*}
-     */
-    fetchAll() {
-        return this.ctx.app.model.User.findAll();
-    }
-
-    /**
-     * 查询分页表格
-     */
-    async fetchList(params) {
-        // 分页查询
-        const list = await this.ctx.app.model.User.findAndCountAll({
-            order: [
-                ['created_at', 'DESC'], // DESC ACS
-            ],
-            // distinct: true, // 数据条数不对
-            where: {
-                name: {
-                    [Op.like]: `%${params.name || ''}%`
-                }
-            },
-            limit: params.pageSize,
-            offset: (params.current - 1) * params.pageSize,
-        });
-        return new Promise((resolve, reject) => {
-            resolve(AppUtils.setListResponse(list));
-        })
-    }
-
-    /**
-     * 查询单个
-     * @param id
-     * @returns {Promise<V> | Promise.<V>}
-     */
-    fetchByName(name) {
-        return this.loader.load(name);
-    }
-
-    /**
-     * 创建用户
-     * @param user
-     * @returns {user}
-     */
     async createUser(user) {
-        const us = await this.ctx.app.model.User.create(user);
-        if (us.dataValues) {
-            return AppUtils.setResponse({
-                message: `${user.name}用户新增成功！`
-            }, 0);
-        } else {
-            return AppUtils.setResponse({
-                message: `${user.name}用户新增失败！`
-            }, 1);
-        }
+        const status = await this.repeatName(user);
+        if (status) return status;
+        return await this.create(user);
     }
 
-    /**
-     * 更新用户
-     * @param user
-     */
     async updateUser(user) {
-        const us = await this.ctx.app.model.User.update(_.pickBy(user), {where: {id: user.id}});
-        if (us.length) {
-            return AppUtils.setResponse({
-                message: `${user.name}用户信息修改成功！`
-            }, 0);
-        } else {
-            return AppUtils.setResponse({
-                message: `${user.name}用户信息修改失败！`
-            }, 1);
-        }
+        return await this.update(user);
     }
 
-    /**
-     * 删除用户
-     */
     async deleteUser(ID) {
-        const delUser = await this.ctx.app.model.User.findOne({where: {id: ID}});
-        if (delUser) {
-            const cacheUser = delUser.toJSON();
-            delUser.destroy();
-            return AppUtils.setResponse({
-                message: `${cacheUser.name}删除成功!`,
-            }, 0);
-        } else {
-            return AppUtils.setResponse({
-                message: `${cacheUser.name}不存在，请检查后重试!`,
-            }, 1);
-        }
+        return await this.delete(ID);
     }
 }
 
---
+++ app/share/table.js
@@ -0,0 +1,159 @@
+'use strict';
+
+const DataLoader = require('dataloader');
+const _ = require('lodash');
+const {Op} = require('sequelize');
+const AppUtils = require('../AppUtils');
+
+class TableConnector {
+    constructor(ctx, app) {
+        this.ctx = ctx;
+        this.init();
+        this.loader = new DataLoader(this.fetch.bind(this));
+    }
+
+    /**
+     * 默认的模型 用于增删改查 继承者需要重写改方法
+     */
+    init() {
+        this.model = null;
+        this.name = '';
+    }
+
+    /**
+     * DataLoader缓存数据
+     * @param ids
+     * @returns {Promise.<*[]>}
+     */
+    fetch(names) {
+        const users = this.model.findAll({
+            where: {
+                name: names,
+            }
+        });
+        return new Promise((resolve, reject) => {
+            users.then(res => {
+                res.length ? resolve(res) : resolve([{}]);
+            })
+        })
+    }
+
+    /**
+     * 查询多个信息
+     * @param names
+     * @returns {Promise.<Array.<V|Error>>|Promise<Array<Error | V>>}
+     */
+    fetchByIds(names) {
+        return this.loader.loadMany(names);
+    }
+
+    /**
+     * 查询所有
+     * @returns {*}
+     */
+    fetchAll() {
+        return this.model.findAll();
+    }
+
+    /**
+     * 查询分页表格
+     */
+    async fetchList(params) {
+        // 分页查询
+        const list = await this.model.findAndCountAll({
+            order: [
+                ['created_at', 'DESC'], // DESC ACS
+            ],
+            // distinct: true, // 数据条数不对
+            where: {
+                name: {
+                    [Op.like]: `%${params.name || ''}%`
+                }
+            },
+            limit: params.pageSize,
+            offset: (params.current - 1) * params.pageSize,
+        });
+        return new Promise((resolve, reject) => {
+            resolve(AppUtils.setListResponse(list));
+        })
+    }
+
+    /**
+     * 查询单个
+     * @param id
+     * @returns {Promise<V> | Promise.<V>}
+     */
+    async fetchByName(name) {
+        return this.loader.load(name);
+    }
+
+    /**
+     * 创建数据
+     * @param user
+     * @returns {user}
+     */
+    async create(rows) {
+        const newRow = await this.model.create(rows);
+        if (newRow.dataValues) {
+            return AppUtils.setResponse({
+                message: `${rows.name}${this.name}新增成功！`
+            }, 0);
+        } else {
+            return AppUtils.setResponse({
+                message: `${rows.name}${this.name}新增失败！`
+            }, 1);
+        }
+    }
+
+    /**
+     * 重名验证
+     * @param user
+     * @returns {*|{data: *}}
+     */
+    async repeatName(user) {
+        const oldRows = await this.model.findOne({where: {name: user.name}});
+        if (oldRows && oldRows.toJSON().name === user.name) {
+            return AppUtils.setResponse({
+                message: `${user.name}${this.name}已存在，请重新输入！`
+            }, 1);
+        }
+    }
+
+    /**
+     * 更新用户
+     * @param user
+     */
+    async update(rows) {
+        const us = await this.model.update(_.pickBy(rows), {where: {id: rows.id}});
+        if (us.length) {
+            return AppUtils.setResponse({
+                message: `${rows.name}${this.name}信息修改成功！`
+            }, 0);
+        } else {
+            return AppUtils.setResponse({
+                message: `${rows.name}${this.name}信息修改失败！`
+            }, 1);
+        }
+    }
+
+    /**
+     * 删除用户
+     */
+    async delete(ID) {
+        const delRows = await this.model.findOne({where: {id: ID}});
+        if (delRows) {
+            const cacheRows = delRows.toJSON();
+            delRows.destroy();
+            return AppUtils.setResponse({
+                message: `${cacheRows.name}删除成功!`,
+            }, 0);
+        } else {
+            return AppUtils.setResponse({
+                message: `${cacheRows.name}不存在，请检查后重试!`,
+            }, 1);
+        }
+    }
+}
+
+module.exports = TableConnector;
+
