afb3fe7df2534ea119e50c35b8fa477fb2a2cdf4
ligaoming <1170234932@qq.com>
2020-10-19 11:17:24 +0800

    合入人：pgli 合入内容： 1、拓扑图数据存储 合入时间：2020-10-19

---
+++ database/migrations/20201019031459-topology.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        /**
+         * Add altering commands here.
+         *
+         * Example:
+         * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+         */
+        const {STRING} = Sequelize;
+
+        await queryInterface.addColumn('topologys', 'image', {
+            // 菜单唯一代码 不可修改 动态生成
+            type: STRING(128),
+        });
+    },
+
+    down: async (queryInterface, Sequelize) => {
+        /**
+         * Add reverting commands here.
+         *
+         * Example:
+         * await queryInterface.dropTable('users');
+         */
+    }
+};
